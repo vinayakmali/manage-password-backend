4aca36f82ea5ce9f54c06a987d38d3aec294b522
Vinayak Mali <vinayak.mali@harbingergroup.com>
2021-10-25 16:46:44 +0530

    bug resolved

--- app/Helpers/base.php
+++ app/Helpers/base.php
@@ -2,7 +2,7 @@
 use DB as DBS;
 function getUsername($text = ""){
 		$username = "";
-		$result = DBS::select("SELECT GROUP_CONCAT(name) as username FROM `users` WHERE id in (2,3,4)");
+		$result = DBS::select("SELECT GROUP_CONCAT(name) as username FROM `users` WHERE id in (".$text.")");
 		if (!empty($result)) {
 			$username = $result[0]->username;
 		}
